f6a87fcf66b90ce66cecd1b684409187dbb69c8d
Personal-IliyanDimov <ilianmd@abv.bg>
2026-03-16 16:32:00 +0200

    Incrementals - V16

--- src/main/java/org/imd/expertschedule/planner/cp/ExpertPlanningConstraintConfiguration.java
+++ src/main/java/org/imd/expertschedule/planner/cp/ExpertPlanningConstraintConfiguration.java
@@ -7,18 +7,22 @@ import org.optaplanner.core.api.score.buildin.hardmediumsoft.HardMediumSoftScore
 @ConstraintConfiguration
 public class ExpertPlanningConstraintConfiguration {
 
-    @ConstraintWeight("Speaker conflict")
-    private HardMediumSoftScore speakerConflict = null;
+    @ConstraintWeight(WeightNames.EA_VS_OA_AVAILABILITY_CONFLICT)
+    private HardMediumSoftScore constraintExpertAvailabilityVsOrderAvailability = HardMediumSoftScore.ONE_HARD;
 
-    @ConstraintWeight("Theme track conflict")
-    private HardMediumSoftScore themeTrackConflict = null;
+    @ConstraintWeight(WeightNames.ES_VS_OS_SKILL_CONFLICT)
+    private HardMediumSoftScore constraintExpertSkillsVsOrderSkills = HardMediumSoftScore.ONE_HARD;
 
-    @ConstraintWeight("Content conflict")
-    private HardMediumSoftScore contentConflict = null;
+    @ConstraintWeight(WeightNames.O_DUE_DATE_CONFLICT)
+    private HardMediumSoftScore constraintOrderDueDate = HardMediumSoftScore.ONE_HARD;
+
+    @ConstraintWeight(WeightNames.FD_SI_CONFLICT)
+    private HardMediumSoftScore constraintFairlyDistributeScheduledItems = HardMediumSoftScore.ONE_MEDIUM;
 
     public static final class WeightNames {
-        private static final String A_CONFLICT = "A_CONFLICT";
-        private static final String B_CONFLICT = "B_CONFLICT";
-        private static final String C_CONFLICT = "C_CONFLICT";
+        private static final String EA_VS_OA_AVAILABILITY_CONFLICT = "ExpertAvailability Vs OrderAvailability";
+        private static final String ES_VS_OS_SKILL_CONFLICT = "ExpertSkills Vs OrderSkills";
+        private static final String O_DUE_DATE_CONFLICT = "OrderDueDate";
+        private static final String FD_SI_CONFLICT = "Fairly Distribute Scheduled Items";
     }
 }
